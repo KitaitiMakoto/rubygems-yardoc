d9698a697bf69187fbbbc35de484ef5e9581763e
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-02 03:14:49 +0900

    Add --yardoc option to gem install command

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,3 +1,22 @@
 require 'rubygems/command_manager'
+require 'rubygems/install_update_options'
+require 'rubygems/commands/yardoc_command'
 
 Gem::CommandManager.instance.register_command :yardoc
+
+module Gem::InstallUpdateOptions
+  alias install_update_options_without_yardoc add_install_update_options
+  def add_install_update_options
+    install_update_options_without_yardoc
+
+    add_option 'Install/Update', '--yardoc', 'Generate YARD documentation for installed gems' do |value, option|
+      unless options[:document].include? 'yardoc'
+        options[:document] << 'yardoc'
+
+        Gem.post_install do |installer|
+          Gem::Commands::YardocCommand.run_yardoc(installer.spec)
+        end
+      end
+    end
+  end
+end

d84773f3d01f33bc0307df616b727c7393cb0b96
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-03-20 17:16:52 +0900

    Follow RubyGems hook mechanism change

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,25 +1,12 @@
-require 'rubygems/command_manager'
-require 'rubygems/install_update_options'
 require 'rubygems/commands/yardoc_command'
 
 Gem::CommandManager.instance.register_command :yardoc
 
-module Gem::InstallUpdateOptions
-  alias install_update_options_without_yardoc add_install_update_options
-  def add_install_update_options
-    install_update_options_without_yardoc
-
-    add_option :'Install/Update', '--yardoc', 'Generate YARD documentation for installed gems' do |value, options|
-      unless options[:document].include? 'yardoc'
-        options[:document] << 'yardoc'
-
-        Gem.post_install do |installer|
-          YARD::Registry.clear
-          Gem::Commands::YardocCommand.run_yardoc(installer.spec)
-        end
-      end
-    end
-  end
+Gem.post_install do |installer|
+  next unless installer.options[:document].include? 'yardoc'
+  next if File.exist? installer.spec.doc_dir('yardoc')
+  YARD::Registry.clear
+  Gem::Commands::YardocCommand.run_yardoc(installer.spec)
 end
 
 Gem.pre_uninstall do |uninstaller|
